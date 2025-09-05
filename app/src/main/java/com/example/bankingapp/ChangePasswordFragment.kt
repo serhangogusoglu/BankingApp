cd87ce2b18fb8244d3cda7f7b9e1ab7b3aa98fcc
Serhan <ggsogluserhan@gmail.com>
2025-09-05 16:38:07 +0300

    ChangePasswordFragment.kt done

--- app/src/main/java/com/example/bankingapp/ChangePasswordFragment.kt
+++ app/src/main/java/com/example/bankingapp/ChangePasswordFragment.kt
@@ -1,18 +1,18 @@
 package com.example.bankingapp
 
+import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import com.example.bankingapp.databinding.FragmentChangePasswordBinding
-import com.example.bankingapp.databinding.FragmentSettingsBinding
 
 class ChangePasswordFragment : Fragment() {
 
     private var _binding: FragmentChangePasswordBinding? = null
     private val binding get() = _binding!!
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,12 +25,45 @@ class ChangePasswordFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val sharedPref = requireContext().getSharedPreferences("UserData", Context.MODE_PRIVATE)
+        val savedPassword = sharedPref.getString("password", "")
 
+        binding.btnBack.setOnClickListener {
+            parentFragmentManager.popBackStack()
+        }
+
+        binding.btnSave.setOnClickListener {
+            val currentPassword = binding.tvCurrentPassword.text.toString().trim()
+            val newPassword = binding.tvFullName.text.toString().trim() // Yeni password alanı
+            val confirmPassword = binding.tvConfirmNewPassword.text.toString().trim()
+
+            if (currentPassword != savedPassword) {
+                Toast.makeText(requireContext(), "Current password is incorrect", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            if (newPassword.length < 6) {
+                Toast.makeText(requireContext(), "New password must be at least 6 characters", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            if (newPassword != confirmPassword) {
+                Toast.makeText(requireContext(), "Passwords do not match", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            // Şifreyi güncelle
+            with(sharedPref.edit()) {
+                putString("password", newPassword)
+                apply()
+            }
+
+            Toast.makeText(requireContext(), "Password updated successfully", Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
-
 }
